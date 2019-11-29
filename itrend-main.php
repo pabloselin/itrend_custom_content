eab3905862cdfcadd60ab38e222c8a147af85eea
Pablo Selin Carrasco Armijo <pablo@apie.cl>
2019-11-29 16:01:33 -0300

    cambio en info del plugin

--- itrend-main.php
+++ itrend-main.php
@@ -1,9 +1,9 @@
 <?php 
 /**
  * Plugin Name:      	Contenido personalizado para visualizacion Itrend
- * Plugin URI:       	https://example.com/plugins/the-basics/
- * Description:      	Handle the basics with this plugin.
- * Version:           	0.0.1
+ * Plugin URI:       	https://itrend.cl
+ * Description:      	Los tipos de contenido, taxonomias y campos personalizados para la visualizacion de actores de Itrend, ademas de algunas utilidades
+ * Version:           	0.0.2
  * Requires at least: 	5.2
  * Requires PHP:      	7.2
  * Author:            	ArtNumerica / APie
@@ -14,7 +14,7 @@
  * Domain Path:       	/lang
  */
 
-define( 'ITREND_PLUGIN_VERSION', '0.0.1' );
+define( 'ITREND_PLUGIN_VERSION', '0.0.2' );
 
 include( plugin_dir_path( __FILE__ ) . 'itrend-custom-fields.php' );
 include( plugin_dir_path( __FILE__ ) . 'itrend-custom-posts.php' );
