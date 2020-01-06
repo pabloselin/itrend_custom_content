6cf7e72caa1b1da2508489e8aab77b3ea64165ca
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-01-06 11:09:08 -0300

    ajuste para urls actores

--- templates/itrend-filters-frontend.php
+++ templates/itrend-filters-frontend.php
@@ -5,8 +5,10 @@ $filter = $_GET['f'];
 if($filter == 'filtro'):
 	include( plugin_dir_path( __FILE__ ) . '/itrend-filters.php');
 elseif($filter == 'visualizacion'):
-	include( plugin_dir_path( __FILE__ ) . 'network.php' );
+	include( plugin_dir_path( __FILE__ ) . '/network.php' );
 	//include( plugin_dir_path( __FILE__ ) . '/itrend-visualization.php');
+else:
+	include( plugin_dir_path( __FILE__ ) . '/itrend-filters.php');
 endif;
 ?>
 
