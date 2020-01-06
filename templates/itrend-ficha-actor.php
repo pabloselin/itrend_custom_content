6033856198022e966b3a2dfb702fe3c59053c09d
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-01-06 11:55:27 -0300

    ajuste ficha boton

--- templates/itrend-ficha-actor.php
+++ templates/itrend-ficha-actor.php
@@ -140,7 +140,9 @@
 		?>
 
 		<div class="link-archive">
-			<a href="<?php echo add_query_arg('f', 'filtro', get_post_type_archive_link( 'actor' ) );?>" class="btn btn-block btn-primary">Volver al buscador de actores</a>
+			<p>
+				<a href="<?php echo add_query_arg('f', 'filtro', get_post_type_archive_link( 'actor' ) );?>" class="btn btn-block btn-primary">Volver al buscador de actores</a>
+			</p>
 		</div>
 	</div>
 </article>
