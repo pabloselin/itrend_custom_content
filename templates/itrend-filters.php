659b34fbc4f073e5774edd6a3d1be9ede7bbd033
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-03-26 17:34:28 -0300

    ajuste botones y filtros

--- templates/itrend-filters.php
+++ templates/itrend-filters.php
@@ -22,13 +22,12 @@
 			</div>
 
 			<div class="filters-info-zone panel">
-				
 				<div class="append-zone-filter">
 					
 				</div>
 				
-				<!-- <button class="btn black" data-action="clean_filters">Quitar filtros</button>
-				<button class="btn red darken-3" data-action="apply_filters">Filtrar</button> -->
+				<button class="btn black" data-action="clean_filters">Quitar filtros</button>
+				<button class="btn red darken-3" data-action="apply_filters">Filtrar</button>
 
 			</div>
 
