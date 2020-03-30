c3d65c689294b479efa0c91b6da0bb36c96e4ca1
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-03-30 17:55:45 -0300

    ajuste altura parrafos itrend introtext

--- itrend-main.php
+++ itrend-main.php
@@ -14,7 +14,7 @@
  * Domain Path:       	/lang
  */
 
-define( 'ITREND_PLUGIN_VERSION', '0.4.5' );
+define( 'ITREND_PLUGIN_VERSION', '0.4.5.1' );
 define( 'ITREND_PREFIX', '_itrend_');
 
 $pluginurl = plugin_dir_url(__FILE__);
